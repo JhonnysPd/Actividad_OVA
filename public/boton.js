20e8672c1179539f6dfe7f49d0d290a3cfc6d407
JoseArgel <pdpc045@gmail.com>
2021-12-03 13:03:49 -0500

    update

--- public/boton.js
+++
@@ -1,8 +0,0 @@
-$(".hero-btn").click(function () {
-  if (!$(".hero-btn").parent().hasClass("active")) {
-    $(this).parent().stop().addClass("active");
-    setTimeout(function () {
-      $(".hero-btn").parent().removeClass("active");
-    }, 2000);
-  }
-});
